31607bd1c4ef3f5ececcd62bd9ec8025e3afdc32
Man Jin <u7167784@anu.edu.au>
2022-03-29 22:04:54 +1100

    new task demo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Form from './Form/Form'
+import Form2 from './Form/New_Task'
 
 function App() {
   return (
     <div className="App">
       <Form style={{}}/>
+      <Form2 style={{}}/>
     </div>
   );
 }
